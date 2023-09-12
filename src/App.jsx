0eb70f95567fd6209cfa64cfcf1ec1cd21623698
Rjprashant713 <guptaprashant082gmail.com>
2023-09-12 10:29:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import TabulatorTable from './components/TabulatorTable'
+
+function App() {
+
+  return (
+    <>
+     
+      <div class="parent">
+      <div class="div1"> </div>
+      <div class="div2"> </div>
+      <div class="div3">  <TabulatorTable /></div>
+      </div>
+    </>
+  )
+}
+
+export default App
